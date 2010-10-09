a036302164bda99312db0981a41d319a413764ea
Claudio Desideri <happy.snizzo@gmail.com>
2010-10-09 18:54:58 +0200

    Trans commit.

---
+++ events.py
@@ -0,0 +1,8 @@
+from direct.showbase.DirectObject import DirectObject
+from panda3d.core import *
+from pandac.PandaModules import *
+from panda3d.ai import *
+
+class EventsManager():
+	def __init__(self, x, y, z,color,owner,parentLegion):
+		
